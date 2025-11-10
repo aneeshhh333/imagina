dd4377885c9576b12b8e7b69a62bef75400a4b37
ANEESH <aneesh06042006@gmail.com>
2025-11-10 21:47:41 +0530

    Update Header.tsx

--- client/my/my/src/components/Header.tsx
+++ client/my/my/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
 
 
 const Header: React.FC = () => {
@@ -8,11 +7,13 @@ const Header: React.FC = () => {
 
   return (
     <header className="header">
+      {/* Left side: logo + name */}
       <div className="logo-section">
         <img src={favicon} alt="Imagina logo" className="logo-icon" />
         <h1 className="logo-text">IMAGINA</h1>
       </div>
 
+      {/* Hamburger menu for mobile */}
       <div
         className={`hamburger ${menuOpen ? "open" : ""}`}
         onClick={() => setMenuOpen(!menuOpen)}
@@ -22,16 +23,17 @@ const Header: React.FC = () => {
         <span></span>
       </div>
 
+      {/* Navigation links */}
       <nav className={`nav-menu ${menuOpen ? "active" : ""}`}>
-        <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>
+        <a href="#home" className="nav-link" onClick={() => setMenuOpen(false)}>
           Home
-        </Link>
-        <Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>
+        </a>
+        <a href="#about" className="nav-link" onClick={() => setMenuOpen(false)}>
           About Us
-        </Link>
-        <Link to="/convert" className="nav-link" onClick={() => setMenuOpen(false)}>
+        </a>
+        <a href="#convert" className="nav-link" onClick={() => setMenuOpen(false)}>
           Converter
-        </Link>
+        </a>
       </nav>
     </header>
   );
